55d087397c86609ceb032e5303e0b006cfa7783d
xy172 <39814521+xy172@users.noreply.github.com>
2023-10-17 20:06:20 -0500

    Add files via upload

--- menus/MenuHelper.gd
+++ menus/MenuHelper.gd
@@ -1,15 +1,29 @@
-extends "res://menus/MenuHelper.gd"
+extends CanvasLayer
+
+signal map_shown
+signal top_menu_changed(top_menu)
+
+var ability_cutscenes:Dictionary
+var tutorials:Dictionary
+var scenes:Dictionary
+
+var _current_top:Node
+var hud:Control
+var sys_layer:CanvasLayer
 
 func drop_random_items():
-	if not DLC.mods_by_id.xy_rle.setting_rle_mod_status:
-		return .drop_random_items()
 	var resources = SaveState.inventory.get_category("resources")
 	var items = []
 	for item_node in resources.get_children():
 		if item_node.is_discardable():
 			var num = randi() % int(max(1, item_node.amount / 10))
-			if num > 0:
+			
+# PATCH: ADD LINES HERE
+			if DLC.mods_by_id.xy_rle.setting_rle_mod_status:
 				num = num * DLC.mods_by_id.xy_rle.setting_rle_modifier_value
+# PATCH: STOP
+			
+			if num > 0:
 				var rec = LootRecord.new()
 				rec.item = item_node.item
 				rec.amount = num
